0e9800bfa770b259a9d5fe17ecdf73655e5e80e6
Elafanta <56441986+Elafanta@users.noreply.github.com>
2020-11-08 21:31:05 +0100

    Übung Async

--- Übung_05/ÜbungAsync.js
+++ Übung_05/ÜbungAsync.js
@@ -2,11 +2,16 @@
 var ÜbungAsync;
 (function (ÜbungAsync) {
     async function communicate(_url) {
+        console.log("vor fetch");
         let response = await fetch(_url);
-        console.log("Response", response);
+        console.log("nach fetch");
+        //await response.text();
+        let ergebnis = await response.text();
+        console.log(ergebnis);
     }
+    console.log("start");
+    communicate("https://jirkadelloro.github.io/EIA2-Inverted/L05_Client/Material/Test.txt");
+    console.log("end");
 })(ÜbungAsync || (ÜbungAsync = {}));
 //function communicate(_url: jirkadelloro.github.io/EIA2-Inverted/L05_Client/Material/Test.txt): void
-function communicate() {
-}
 //# sourceMappingURL=ÜbungAsync.js.map
