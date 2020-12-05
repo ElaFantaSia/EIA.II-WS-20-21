51c1fca9ba1db2f3b03c7892d53750013ab8f377
Elafanta <56441986+Elafanta@users.noreply.github.com>
2020-12-05 22:05:49 +0100

    alley and finish homework L07

---
+++ Aufgabe_08/test.ts
@@ -0,0 +1,18 @@
+let canvas: HTMLCanvasElement = document.querySelector("canvas");
+let crc2: CanvasRenderingContext2D = canvas.getContext("2d");
+
+//crc2.fillStyle = "#FF0000";
+//crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+//crc2.beginPath();
+//crc2.arc(100, 100, 20, 0, 1.5 * Math.PI);
+crc2.beginPath();
+crc2.moveTo(2.1, 1);
+crc2.lineTo(2.1, 10);
+crc2.moveTo(4.5, 1);
+crc2.lineTo(4.5, 10);
+crc2.moveTo(7.5, 1);
+crc2.lineTo(10.5, 10);
+crc2.stroke();
+
+crc2.closePath();
+crc2.stroke();
