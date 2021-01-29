114f4487e8de0babfee494a20415c8536a01968f
Elafanta <56441986+Elafanta@users.noreply.github.com>
2021-01-29 20:22:11 +0100

    startTest

---
+++ Aufgabe_03/TestAbgabe/main.ts
@@ -0,0 +1,19 @@
+window.addEventListener("load", handleLoad);
+
+var firework;
+(function (1_firework) {
+    window.addEventListener("load", handleLoad);
+    let crc2;
+
+/* (function handleLoad(_event) {
+    console.log("handleLoad");
+    let firework: Firework[] = [];
+} */
+}
+
+function handleLoad(_event) {
+    let canvas = document.querySelector("canvas");
+    if (!canvas)
+        return;
+    crc2 = canvas.getContext("2d");
+}
